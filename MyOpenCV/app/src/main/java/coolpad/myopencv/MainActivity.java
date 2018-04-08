794f119a504b6fc5a27744f97eb0d82da1277c8b
linronghui <frankie.lin@yulong.com>
2018-04-08 17:42:27 +0800

    get picture brightness OK
    Signed-off-by: linronghui <frankie.lin@yulong.com>

--- MyOpenCV/app/src/main/java/coolpad/myopencv/MainActivity.java
+++ MyOpenCV/app/src/main/java/coolpad/myopencv/MainActivity.java
@@ -23,9 +23,14 @@ import org.opencv.imgproc.Imgproc;
 public class MainActivity extends AppCompatActivity {
 
     Button btnProcess;
+    Button btnBrightness;
     Bitmap srcBitmap;
     Bitmap grayBitmap;
     ImageView imgHuaishi;
+
+    private double sum_of_gray_pixels = 0;
+    private double avg_of_gray_pixels = 0;
+    private int calc_gray_pixel[] = new int[256];
     private static boolean flag = true;
     //private static boolean isFirst = true;
     private static final String TAG = "MainActivity";
@@ -49,6 +54,77 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+    private class BrightnessClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Mat rgbMat = new Mat();
+                    Mat grayMat = new Mat();
+
+                    srcBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.clear_220_220_new_dark);
+                    grayBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
+                    Utils.bitmapToMat(srcBitmap, rgbMat);//convert original bitmap to Mat, R G B.
+
+                    Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);//rgbMat to gray grayMat
+
+                    Utils.matToBitmap(grayMat, grayBitmap); //convert mat to bitmap
+
+
+                    for (int i = 0; i < 256; i++) calc_gray_pixel[i] = 0;
+
+                    int m_Height = srcBitmap.getHeight();
+                    int m_Width = srcBitmap.getWidth();
+
+                    Log.i(TAG, "m_Height is" + m_Height);
+                    Log.i(TAG, "m_Width is" + m_Width);
+
+                    for (int kk = 0; kk < m_Height; kk++) {
+                        for (int aa = 0; aa < m_Width; aa++) {
+                            double gray_pixel_value[] = grayMat.get(aa, kk);
+                            sum_of_gray_pixels += (gray_pixel_value[0] - 128);
+
+                            int x = (int) gray_pixel_value[0];
+                            calc_gray_pixel[x]++;
+                        }
+                    }
+
+                    Log.i(TAG, "ooooookkkkkk1111");
+
+                    avg_of_gray_pixels = sum_of_gray_pixels / (grayBitmap.getHeight() * grayBitmap.getWidth());
+
+                    double total = 0;
+                    double mean = 0;
+
+                    for (int i = 0; i < 256; i++) {
+                        total += Math.abs(i - 128 - avg_of_gray_pixels) * calc_gray_pixel[i];
+                    }
+
+                    Log.i(TAG, "ooooookkkkkk22222");
+                    mean = total / (grayBitmap.getHeight() * grayBitmap.getWidth());
+                    double cast = Math.abs(avg_of_gray_pixels / mean);
+
+                    //            Log.i(TAG, "light: %f", cast);
+
+
+
+                    if (cast > 1) {
+                        if (avg_of_gray_pixels > 0) {
+                            Log.i(TAG, "too light");
+                        } else
+                            Log.i(TAG, "too dark");
+                    } else
+                        Log.i(TAG, "brightness normal!!");
+                }
+            }).start();
+
+            imgHuaishi.setImageBitmap(grayBitmap);
+            Log.i(TAG, "over!!");
+
+        }
+    }
     private class ProcessClickListener implements View.OnClickListener {
 
         @Override
@@ -106,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void initUI(){
         btnProcess = (Button)findViewById(R.id.btn_gray_process);
+        btnBrightness = (Button)findViewById(R.id.btn_brightness);
         imgHuaishi = (ImageView)findViewById(R.id.img_huaishi);
         Log.i(TAG, "initUI sucess...");
 
@@ -118,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
 
         initUI();
         btnProcess.setOnClickListener(new ProcessClickListener());
+        btnBrightness.setOnClickListener(new BrightnessClickListener());
 
     }
 
